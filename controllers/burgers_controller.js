d57f02af5ae93b67c5a35b6b91475153fffd0b6e
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2020-12-11 23:39:51 -0700

    Added CSS

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,7 @@ router.get("/", function (req, res) {
     var hbsObject = {
       burgers: data,
     };
-    console.log(hbsObject);
+
     res.render("index", hbsObject);
   });
 });
@@ -25,8 +25,6 @@ router.post("/api/burgers", function (req, res) {
 
 router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
 
   burger.updateOne(
     {
