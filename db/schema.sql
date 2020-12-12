05d20d762a4844e7e1a8d66487548beef95891b5
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2020-12-11 20:02:57 -0700

    Initial Commit

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(30) NULL,
+  devoured BOOLEAN DEFAULT 0,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Lamb Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beyond Burger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('1/4er-lber Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
