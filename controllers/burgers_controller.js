c9f2773c4b1732375e7058e93a79016501afc245
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2020-12-11 21:00:41 -0700

    Routes working on postman

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-  cat.insertOne(["name"], [req.body.name], function (result) {
+  burger.insertOne(["burger_name"], [req.body.name], function (result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -28,7 +28,7 @@ router.put("/api/burgers/:id", function (req, res) {
 
   console.log("condition", condition);
 
-  cat.update(
+  burger.updateOne(
     {
       devoured: req.body.devoured,
     },
